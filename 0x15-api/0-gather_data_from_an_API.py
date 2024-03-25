5c430908bbe9c3452211b616bcd0cd46d13c72e4
Muna-Yusuf <muna.y.a.jim@gamil.com>
2024-03-25 14:25:25 -0700

    ....

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,51 +1,27 @@
 #!/usr/bin/python3
-"""Python script that, using this REST API, for a given employee ID,
-   returns information about his/her TODO list progress.
-
-   Requirements:
-    - You must use urllib or requests module.
-    - The script must accept an integer as a parameter,
-      which is the employee ID.
-    - The script must display on the standard output the..
-      employee TODO list progress in this exact format:
-
-        - First line: Employee EMPLOYEE_NAME is done with...
-          tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-            * EMPLOYEE_NAME: name of the employee.
-            * NUMBER_OF_DONE_TASKS: number of completed tasks.
-            * TOTAL_NUMBER_OF_TASKS: total number of tasks, which...
-              is the sum of completed and non-completed tasks.
-        - Second and N next lines display the title of completed tasks:
-          TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE).
-"""
-
+""" module doc """
 import requests
 import sys
 
-url = 'https://jsonplaceholder.typicode.com/'
 
-
-def check_tasks(id):
+def main():
+    """ def com """
     id = sys.argv[1]
-    user_resp = requests.get(url + "users/{}".format(id))
-    user = user_resp.json()
-    params = {"userId": id}
-    task_req = requests.get(url + "todos", params=params)
-    task_res = task_req.json()
-    completed_task = []
-
-    for task in task_res:
-        if task.get("completed") is True:
-            completed_task.append(task.get("title"))
-
-    print("Employee {} is done with tasks({}/{}):".format(
-                    user.get("name"),
-                    len(completed_task),
-                    len(task_res)
-                ))
-    for completed in completed_task:
-        print("\t {}".format(completed))
+    url = f'https://jsonplaceholder.typicode.com/'
+    users = f'users?id={id}'
+    todos = f'todos?userId={id}'
+    done = f'{todos}&completed=true'
+    notDone = f'{todos}&completed=false'
+    userData = requests.get(f'{url}{users}').json()
+    Name = userData[0].get("name")
+    todosData = requests.get(f'{url}{todos}').json()
+    todosDone = requests.get(f'{url}{done}').json()
+    doneN = len(todosDone)
+    totalN = len(todosData)
+    print(f'Employee {Name} is done with tasks({doneN}/{totalN}):')
+    for task in todosDone:
+        print("\t "+task.get("title"))
 
 
 if __name__ == "__main__":
-    check_tasks(int(sys.argv[1]))
+    main()
